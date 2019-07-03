57e930896770dda831a527410c01a740bc76f264
AleksandrSpodin <a@spodin.com>
2019-07-03 08:46:13 +0300

    refactoring with parametrized tests

--- src/test/java/converter/RomanArabicConverterTest.java
+++ src/test/java/converter/RomanArabicConverterTest.java
@@ -1,31 +1,17 @@
 package converter;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanArabicConverterTest {
 
-    @DisplayName("0 -> empty string")
-    @Test
-    void testConvertZeroToEmptyString() {
-        String romanNumber = convertToRoman(0);
-        assertEquals("", romanNumber);
-    }
-
-    @DisplayName("1 -> I")
-    @Test
-    void testConvertOneToI() {
-        String romanNumber = convertToRoman(1);
-        assertEquals("I", romanNumber);
-    }
-
-    @DisplayName("2 -> II")
-    @Test
-    void testConvertTwoToII() {
-        String romanNumber = convertToRoman(2);
-        assertEquals("II", romanNumber);
+    @ParameterizedTest
+    @CsvSource(value = {"0:''", "1:I", "2:II"}, delimiter = ':')
+    void testConvertArabicToRoman(int input, String expected) {
+        String romanNumber = convertToRoman(input);
+        assertEquals(expected, romanNumber);
     }
 
     private String convertToRoman(int arabicNumber) {
