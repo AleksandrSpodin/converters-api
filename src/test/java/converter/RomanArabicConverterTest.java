4782caed721ede4ad9bc8039b7f5a97d0adee1e6
AleksandrSpodin <a@spodin.com>
2019-07-03 08:59:37 +0300

    add test 6->VI passed

--- src/test/java/converter/RomanArabicConverterTest.java
+++ src/test/java/converter/RomanArabicConverterTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RomanArabicConverterTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"0:''", "1:I", "2:II", "3:III", "5:V"}, delimiter = ':')
+    @CsvSource(value = {"0:''", "1:I", "2:II", "3:III", "5:V", "6:VI"}, delimiter = ':')
     void testConvertArabicToRoman(int input, String expected) {
         String romanNumber = convertToRoman(input);
         assertEquals(expected, romanNumber);
